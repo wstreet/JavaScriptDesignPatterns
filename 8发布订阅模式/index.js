a152a1ee9af462ebffd849d0e5a985b662b05457
wstreet7 <wstreet7@outlook.com>
2019-08-20 00:47:41 +0800

    发布订阅模式

---
+++ 8发布订阅模式/index.js
@@ -0,0 +1,29 @@
+class Event {
+    _default = 'default'
+    _slice = Array.prototype.slice
+    _shift = Array.prototype.shift
+    _unshift = Array.prototype._unshift
+    namespaceCache = {}
+
+    // 
+    _create(namespace) {
+
+    }
+
+    // 添加订阅
+    _listen() {
+
+    }
+
+    // 发布
+    _trigger() {
+
+    }
+
+    // 删除订阅
+    _remove() {
+
+    }
+
+
+}
