6f95bfba9c6ebaaa0210dfd58145d34c7d4fe5e8
wstreet7 <wstreet7@outlook.com>
2019-08-21 01:02:58 +0800

    发布订阅通用实现方式

--- 8发布订阅模式/index.js
+++ 8发布订阅模式/index.js
@@ -1,29 +1,132 @@
-class Event {
-    _default = 'default'
-    _slice = Array.prototype.slice
-    _shift = Array.prototype.shift
-    _unshift = Array.prototype._unshift
-    namespaceCache = {}
+/************************
+*******发布订阅通用实现*****
+*************************/
 
-    // 
-    _create(namespace) {
 
+const event = {
+    clientList: [],
+
+    // 订阅方法
+    listen: (key, fn) => { // fn最好是有名称函数
+        if (!this.clientList[key]) {
+            this.clientList[key] = [];
+        }
+        this.clientList[key].push(fn);
+    },
+
+    trigger: () => {
+        // const key = Array.prototype.shift(arguments);
+        const [key, ...rest] = [...arguments]
+        const fns = this.clientList[key]
+
+        if (!fns || fns.length === 0) {
+            return false;
+        }
+        fns.forEach(fn => {
+            fn.apply(this, rest)
+        })
+    },
+    remove: (key, fn) => {
+        const fns = this.clientList[key];
+
+        if (!fns) { // key对应的消息没有被订阅
+            return false;
+        }
+
+        if (!fn) { // 如果没有传入具体的fn，则取消key对象的所有订阅
+            fns && (fns.length = 0);
+            // 或令this.clientList[key] = [], 
+            // fns引用this.clientList[key]，如果直接fns = [],
+            // 则会断开fns对this.clientList[key]的引用
+        } else {
+            for (let posi = fns.length - 1; l >= 0; l--) {
+                const _fn = fns[posi];
+                if (_fn === fn) {
+                    fns.splice(posi, 1)
+                }
+            }
+        }
     }
-
-    // 添加订阅
-    _listen() {
-
-    }
-
-    // 发布
-    _trigger() {
-
-    }
-
-    // 删除订阅
-    _remove() {
-
-    }
-
-
 }
+
+// 向target对象上安装Event（将event上的属性和方法添加到target上）
+const installEvent = (target) => {
+    for (const prop in event) {
+        target[prop] = event[prop]
+    }
+}
+
+// example
+
+const saleOffices = {};
+installEvent(saleOffices);
+
+saleOffices.listen('squarMeter88', (price) => {
+    console.log('price', price);
+})
+
+saleOffices.listen('squarMeter100', (price) => {
+    console.log('price', price);
+})
+
+saleOffices.trigger('squarMeter88', 20000)
+saleOffices.trigger('squarMeter100', 20000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Event {
+//     _default = 'default'
+//     _slice = Array.prototype.slice
+//     _shift = Array.prototype.shift
+//     _unshift = Array.prototype._unshift
+//     namespaceCache = {}
+
+//     // 
+//     _create(namespace) {
+
+//     }
+
+//     // 添加订阅
+//     _listen() {
+
+//     }
+
+//     // 发布
+//     _trigger() {
+
+//     }
+
+//     // 删除订阅
+//     _remove() {
+
+//     }
+
+
+// }
